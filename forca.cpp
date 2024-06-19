e9ebc8c834163b02b876dc8e14298275dc209478
emanuelcgalindo@hotmail.com <emanuelcgalindo@hotmail.com>
2024-06-19 10:02:36 -0300

    primeiro envio

---
+++ forca.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <set>
+
+std ::vector<std::string> retornaCoutendoArquivo(const std::string &nomeArquivo) {
+    std::vector<std::string>palavras;
+    std::ifstream arquivo(nomeArquivo);
+    std:string palavra;
+
+    if(arquivo.is_open()) {
+        while (getline(arquivo, palavra)) {
+            palavras.push_back(palavra);
+        }
+
+        arquivo.close();
+        
+    } else {
+        std::cerr <<"Erro ao abrir o arquivo " <<nomeArquivo << std::endl;
+
+}
+return palavras
+
+
+}
